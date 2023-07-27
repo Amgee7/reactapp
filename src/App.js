a7e64014e159e3fd87169fcc10e4cc6620875de5
Amgee7 <amgee.shrestha@gmail.com>
2023-07-27 11:40:03 +0545

    new message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello! This is Amgee...
+          Hello! This is Amgee..
         </a>
       </header>
     </div>
